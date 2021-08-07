22fe5814401259071fe060320793955f05392490
Ryan McCullagh <ryan@amezmo.com>
2021-08-07 14:32:37 -0500

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,8 +23,14 @@ namespace dotnet_core.Controllers
                 WriteIndented = true
             };
 
+            // https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/proxy-load-balancer?view=aspnetcore-5.0#forwarded-headers
             return new JsonResult(new {
-                Client = Request.HttpContext.Connection.RemoteIpAddress,
+                // X-Forwarded-For
+                Client = HttpContext.Connection.RemoteIpAddress,
+                // Host X-Forwarded-Host
+                Host = HttpContext.Request.Host,
+                // X-Forwarded-Proto
+                XForwardedForScheme = HttpContext.Request.Scheme,
                 IsHttps = Request.IsHttps,
                 Environment = webHostEnvironment.EnvironmentName,
                 Headers = Request.Headers.ToDictionary(keyValuePair => keyValuePair.Key, keyValuePair => keyValuePair.Value)
